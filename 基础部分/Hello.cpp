9e368a452b39247ca9c8cf8a079778ddc3526ba7
LeslieLiu16 <2596943294@qq.com>
2022-07-09 22:22:24 +0800

    尝试了几个新的函数

--- 基础部分/Hello.cpp
+++ 基础部分/Hello.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+int add(int a, int b)
+{
+
+    return a + b;
+}
+
+void exchange(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+bool is_equal(int a, int b)
+{
+    return a == b ? true : false;
+}
+
+void all_multiply()
+{
+    int a;
+    cout << "请输入数字："
+         << " ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        if (0 == a % i)
+        {
+            cout << i << "*";
+            cout << a / i << "= ";
+            cout << a << endl;
+        }
+    }
+}
+
 
 int main()
 {
-    cout << "Hello,world!" << endl;
+    int a, b;
+    a = 168;
+    b = 20;
+
+    cout << "交换前的a和b:" << a << " ";
+    cout << b << endl;
+
+    exchange(a, b);
+
+    cout << "交换后的a和b:" << a << " ";
+    cout << b << endl;
+
+    cout << is_equal(a, b) << endl;
+
+    all_multiply();
+
     system("pause");
     return 0;
 }
