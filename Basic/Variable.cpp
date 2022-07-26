3e6b230ffcbd130773bc1d66cca226206de6fbe0
LeslieLiu16 <2596943294@qq.com>
2022-07-26 21:54:08 +0800

    update

--- Basic/Variable.cpp
+++ Basic/Variable.cpp
@@ -3,19 +3,33 @@ using namespace std;
 
 extern int d;
 void func();
+#define PI 3.1415;
+float area(int R);
 
+const float e = 2.78;
 
+int main()
+{
 
-int main(){
-    
     int d;
     d = 100;
     cout << d << endl;
-    func();
+    // func();
+
+    cout << "area is:" << area(3) << endl;
+    cout << "e is:" << e << endl;
     system("pause");
     return 0;
 }
 
-void func(){
+void func()
+{
     cout << "2333" << endl;
+}
+
+float area(int R)
+{
+
+    float res = R * R * PI;
+    return res;
 }
