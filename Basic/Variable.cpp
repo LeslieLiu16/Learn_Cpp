c4d5341d1d72ac492fbb872bc351c6a7e9d0a132
LeslieLiu16 <2596943294@qq.com>
2022-07-26 21:27:26 +0800

    update

--- Basic/Variable.cpp
+++ Basic/Variable.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 extern int d;
+void func();
+
+
 
 int main(){
     
     int d;
     d = 100;
     cout << d << endl;
+    func();
     system("pause");
     return 0;
+}
+
+void func(){
+    cout << "2333" << endl;
 }
